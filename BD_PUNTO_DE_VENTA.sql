4c50fa148907cba0ca90e3cbf26c6660964dd767
Lucasabdala94 <lucasabdala94@gmail.com>
2024-04-04 04:07:15 -0300

    agregado mas cosas

---
+++ BD_PUNTO_DE_VENTA.sql
@@ -0,0 +1,79 @@
+create database bd_sistemas_ventas;
+
+use bd_sistemas_ventas;
+
+-- crear tabla usuarios
+create table tb_usuario(
+idUsuario int (11) auto_increment primary key,
+nombre varchar(30) not null,
+apellido varchar(30) not null,
+usuario varchar(15) not null,
+password varchar(15) not null,
+telefono varchar(15) not null,
+estado int(1) not null 
+);
+-- crear tabla clientes
+create table tb_cliente(
+idCliente int (11) auto_increment primary key,
+nombre varchar(30) not null,
+apellido varchar(30) not null,
+cedula varchar(15) not null,
+telefono varchar(15) not null,
+direccion varchar(100) not null,
+estado int(1) not null 
+);
+-- crear tabla categorias
+create table tb_categoria(
+idcategoria int (11) auto_increment primary key,
+descripcion varchar(200) not null,
+estado int(1) not null 
+);
+-- crear tabla productos
+create table tb_producto(
+idProducto int (11) auto_increment primary key,
+nombre varchar(100) not null,
+cantidad int(11) not null,
+precio double(10,2) not null,
+descripcion varchar(200) not null,
+porcentajeIva int(2) not null,
+idCategoria int(11) not null,
+estado int(1) not null 
+);
+
+-- crear tabla cabecera de venta
+create table tb_cabecera_venta(
+idCabeceraVenta int (11) auto_increment primary key,
+idCliente int(11) not null,
+valorPagar double(10,2) not null,
+fechaVenta date not null,
+estado int(1) not null 
+);
+
+-- crear tabla detalle de venta
+create table tb_detalle_venta(
+idDetalleVenta int (11) auto_increment primary key,
+idCabeceraVenta int(11) not null,
+idProducto int(11) not null,
+cantidad int(11) not null,
+precioUnitario double(10,2) not null,
+subtotal double(10,2) not null,
+descuento double(10,2) not null,
+iva double(10,2) not null,
+totalPagar double(10,2) not null,
+estado int(1) not null 
+);
+
+insert into tb_usuario(nombre,apellido,usuario,password,telefono,estado)
+values("Lucas","Abdala","catepecu","123456","03816411006",1);
+
+select usuario, password from tb_usuario 
+where usuario = "catepecu" and password="123456";
+
+ show tables;
+ 
+select * from tb_usuario;
+select * from tb_categoria;
+truncate table tb_categoria;
+select descripcion from tb_categoria where descripcion='frutas';
+
+
---
+++ sql/bd_puntoVentas.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  PUESTO 2
+ * Created: 4 abr 2024
+ */
+
